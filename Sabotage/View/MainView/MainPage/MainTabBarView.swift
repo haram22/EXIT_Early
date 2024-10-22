b9528f3300f15ba54f130dcb55b0df7bdbadd0e0
haram22 <kimhr9889@handong.ac.kr>
2024-10-22 15:56:14 +0900

    [Fix] mainvc snapkit, then 적용 및 코드분리

--- Sabotage/View/MainView/MainPage/MainTabBarView.swift
+++ Sabotage/View/MainView/MainPage/MainTabBarView.swift
@@ -6,3 +6,105 @@
 //
 
 import Foundation
+import UIKit
+
+extension MainVC {
+    func toggleUI() {
+        view.addSubview(actionTogglebuttonTapped)
+        actionTogglebuttonTapped.contentMode = .scaleAspectFit
+        actionTogglebuttonTapped.isHidden = false
+        
+        view.addSubview(limitTogglebuttonTapped)
+        limitTogglebuttonTapped.contentMode = .scaleAspectFit
+        limitTogglebuttonTapped.isHidden = true
+        
+        view.addSubview(leftButton)
+        leftButton.setTitle("", for: .normal)
+        leftButton.addTarget(self, action: #selector(leftButtonTapped), for: .touchUpInside)
+        leftButton.addTarget(self, action: #selector(leftButtonTapped), for: .touchUpInside)
+        
+        view.addSubview(rightButton)
+        rightButton.setTitle("", for: .normal)
+        rightButton.addTarget(self, action: #selector(rightButtonTapped), for: .touchUpInside)
+        rightButton.addTarget(self, action: #selector(rightButtonTapped), for: .touchUpInside)
+    }
+    
+    func toggleConstraintUI() {
+        actionTogglebuttonTapped.then {
+            view.addSubview($0)
+            $0.snp.makeConstraints {
+                $0.top.equalTo(pieChartBG.snp.bottom).offset(-10)
+                $0.centerX.equalToSuperview()
+                $0.width.equalTo(view.frame.width)
+                $0.height.equalTo(60)
+            }
+        }
+        
+        limitTogglebuttonTapped.then {
+            view.addSubview($0)
+            $0.snp.makeConstraints {
+                $0.top.equalTo(pieChartBG.snp.bottom).offset(-10)
+                $0.centerX.equalToSuperview()
+                $0.width.equalTo(view.frame.width)
+                $0.height.equalTo(60)
+            }
+        }
+        
+        leftButton.then {
+            view.addSubview($0)
+            $0.snp.makeConstraints {
+                $0.top.equalTo(pieChartBG.snp.bottom).offset(-10)
+                $0.leading.equalTo(actionTogglebuttonTapped.snp.leading)
+                $0.width.equalTo(200)
+                $0.height.equalTo(60)
+            }
+        }
+        
+        rightButton.then {
+            view.addSubview($0)
+            $0.snp.makeConstraints {
+                $0.top.equalTo(pieChartBG.snp.bottom).offset(-10)
+                $0.trailing.equalTo(actionTogglebuttonTapped.snp.trailing)
+                $0.width.equalTo(200)
+                $0.height.equalTo(60)
+            }
+        }
+    }
+
+    
+    @objc func leftButtonTapped() {
+        if actionTogglebuttonTapped.isHidden {
+            // actionTogglebuttonTapped이 숨겨져 있는 경우에만 작동하도록 설정
+            toggleCondition()
+        }
+    }
+    
+    // actionTogglebuttonTapped 버튼을 눌렀을 때 실행되는 메서드
+    @objc func rightButtonTapped() {
+        if limitTogglebuttonTapped.isHidden {
+            // limitTogglebuttonTapped이 숨겨져 있는 경우에만 작동하도록 설정
+            toggleCondition()
+        }
+    }
+    
+    func toggleCondition() {
+        if actionTogglebuttonTapped.isHidden {
+            // actionTogglebuttonTapped is hidden: show actionButton and hide limitButton
+            actionTogglebuttonTapped.isHidden = false
+            limitTogglebuttonTapped.isHidden = true
+            actionButton.isHidden = false
+            limitButton.isHidden = true
+            //                actionTableView.isHidden = false
+            //                limitTableView.isHidden = true
+        } else {
+            // actionTogglebuttonTapped is visible: hide actionButton and show limitButton
+            actionTogglebuttonTapped.isHidden = true
+            limitTogglebuttonTapped.isHidden = false
+            actionButton.isHidden = true
+            limitButton.isHidden = false
+            //                actionTableView.isHidden = true
+            //                limitTableView.isHidden = false
+        }
+    }
+    
+}
--- Sabotage/View/MainView/MainPage/MainVC.swift
+++ Sabotage/View/MainView/MainPage/MainVC.swift
@@ -1,6 +1,3 @@
-//Main
-
-//MainVC.swift - 메인 페이지
 
 import UIKit
 import SwiftUI
@@ -45,7 +42,7 @@ class MainVC: UIViewController{
         addChild(pieChartViewController)
         view.addSubview(pieChartViewController.view)
         pieChartViewController.didMove(toParent: self)
-        
+
         pieChartViewController.view.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
@@ -55,24 +52,19 @@ class MainVC: UIViewController{
     }
     
     func top3Apps() {
-        let scheduleVM = ScheduleVM() // Set up your ScheduleVM instance with necessary configurations
-        
+        let scheduleVM = ScheduleVM()
         let monitoringView = MonitoringView().environmentObject(scheduleVM)
         let hostingController = UIHostingController(rootView: monitoringView)
         addChild(hostingController)
         pieChartBG.addSubview(hostingController.view)
         
         hostingController.view.backgroundColor = .base100
-        // Set constraints for the HostingController's view using Auto Layout or other layout methods
         hostingController.view.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             hostingController.view.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
-            //            hostingController.view.leadingAnchor.constraint(equalTo: pieChartBG.centerXAnchor, constant: 10),
             hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 220),
             hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
             hostingController.view.bottomAnchor.constraint(equalTo: view.topAnchor, constant: 280),
-            //            hostingController.view.widthAnchor.constraint(equalToConstant: 150),
-            //            hostingController.view.heightAnchor.constraint(equalToConstant: 200),
         ])
         hostingController.didMove(toParent: self)
     }
@@ -108,62 +100,72 @@ class MainVC: UIViewController{
     }
     
     func pieChartConstraintUI() {
-        
-        logoImageView.translatesAutoresizingMaskIntoConstraints = false
-        logoText.translatesAutoresizingMaskIntoConstraints = false
-        pieChartBG.translatesAutoresizingMaskIntoConstraints = false
-        today.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.then {
+            view.addSubview($0)
+            $0.snp.makeConstraints {
+                $0.top.equalTo(view.safeAreaLayoutGuide).offset(20.95) // 상단 여백
+                $0.leading.equalToSuperview().offset(25.15) // 좌측 여백
+                $0.width.equalTo(39.304) // 가로 크기
+                $0.height.equalTo(39.6) // 세로 크기
+            }
+        }
 
-        forMoreAnalysis.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20.95), // 상단에 여백을 줄 수 있도록 조정
-            logoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25.15), // 좌측에 여백을 줄 수 있도록 조정
-            logoImageView.widthAnchor.constraint(equalToConstant: 39.304), // 이미지의 가로 크기 조정
-            logoImageView.heightAnchor.constraint(equalToConstant: 39.6), // 이미지의 세로 크기 조정
-            
-            logoText.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 31), // 상단에 여백을 줄 수 있도록 조정
-            logoText.leadingAnchor.constraint(equalTo: logoImageView.leadingAnchor, constant: 45), // 좌측에 여백을 줄 수 있도록 조정
-            logoText.widthAnchor.constraint(equalToConstant: 63), // 이미지의 가로 크기 조정
-            logoText.heightAnchor.constraint(equalToConstant: 20), // 이미지의 세로 크기 조정
-            
-            pieChartBG.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 16),
-            pieChartBG.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            pieChartBG.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            pieChartBG.widthAnchor.constraint(equalToConstant: 400), // 우측에 여백을 줄 수 있도록 조정
-            pieChartBG.heightAnchor.constraint(equalToConstant: 300), // 이미지의 세로 크기 조정
-            
-            today.topAnchor.constraint(equalTo: pieChartBG.topAnchor, constant: 34),
-            today.trailingAnchor.constraint(equalTo: pieChartBG.leadingAnchor, constant: 140),
-            today.widthAnchor.constraint(equalToConstant: 74),
-            today.heightAnchor.constraint(equalToConstant: 22),
-            
-            forMoreAnalysis.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 290), // 상단에 여백을 줄 수 있도록 조정
-            forMoreAnalysis.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            //            forMoreAnalysis.leadingAnchor.constraint(equalTo: logoImageView.leadingAnchor, constant: 45),
-            forMoreAnalysis.widthAnchor.constraint(equalToConstant: 80),
-            forMoreAnalysis.heightAnchor.constraint(equalToConstant: 30),
-        ])
-        
+        logoText.then {
+            view.addSubview($0)
+            $0.snp.makeConstraints {
+                $0.top.equalTo(view.safeAreaLayoutGuide).offset(31) // 상단 여백
+                $0.leading.equalTo(logoImageView.snp.trailing).offset(45) // 좌측 여백
+                $0.width.equalTo(63) // 가로 크기
+                $0.height.equalTo(20) // 세로 크기
+            }
+        }
+
+        pieChartBG.then {
+            view.addSubview($0)
+            $0.snp.makeConstraints {
+                $0.top.equalTo(logoImageView.snp.bottom).offset(16) // 상단 여백
+                $0.leading.equalToSuperview().offset(10) // 좌측 여백
+                $0.trailing.equalToSuperview().offset(-10) // 우측 여백
+                $0.width.equalTo(400) // 가로 크기
+                $0.height.equalTo(300) // 세로 크기
+            }
+        }
+
+        today.then {
+            view.addSubview($0)
+            $0.snp.makeConstraints {
+                $0.top.equalTo(pieChartBG).offset(34) // 상단 여백
+                $0.trailing.equalTo(pieChartBG.snp.leading).offset(140) // 우측 여백
+                $0.width.equalTo(74) // 가로 크기
+                $0.height.equalTo(22) // 세로 크기
+            }
+        }
+
+        forMoreAnalysis.then {
+            view.addSubview($0)
+            $0.snp.makeConstraints {
+                $0.top.equalTo(view.safeAreaLayoutGuide).offset(290) // 상단 여백
+                $0.centerX.equalToSuperview() // 중앙 정렬
+                $0.width.equalTo(80) // 가로 크기
+                $0.height.equalTo(30) // 세로 크기
+            }
+        }
     }
     
     @objc func forMoreAnalysisTapped() {
-        // Navigate to AnalysisVC
-        let analysisVC = ActionTableView_RAM() // Assuming AnalysisVC is your destination view controller
+        let analysisVC = ActionTableView_RAM()
         navigationController?.pushViewController(analysisVC, animated: true)
     }
     
-   
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .base50
         
         pieChartViewUI()
         pieChartConstraintUI()
         piechartUI()
-     
+        toggleUI()
+        toggleConstraintUI()
         
         // 이전 화면으로 돌아가는 "< Back" 버튼 숨기기
         navigationItem.hidesBackButton = true
@@ -175,30 +177,27 @@ class MainVC: UIViewController{
         actionButton.setImage(UIImage(named: "main_actionButton.png"), for: .normal)
         actionButton.contentMode = .scaleAspectFit
         actionButton.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
-        //        view.addSubview(actionButton)
         actionButton.translatesAutoresizingMaskIntoConstraints = false
         actionButton.isHidden = false
-        
-        // actionTableView의 푸터 뷰로 actionButton을 설정
-        
     }
+    
     @objc func showPage(_ notification:Notification) {
-            if let userInfo = notification.userInfo {
-                if let index = userInfo["index"] as? Int {
+        if let userInfo = notification.userInfo {
+            if let index = userInfo["index"] as? Int {
                 
-                    // 네번째 탭의 VC는 NavigationBar를 가지고 있어서 UINavigationController로 다운 캐스팅을 해주기
-                    let navigationController = self.children[index] as? UINavigationController
-                    
-                    
-                    // navigationController에 연결되어 있는 secondVC를 push 형식으로 전환
-                    guard let secondVC = storyboard?.instantiateViewController(withIdentifier: "secondVC") as? BeforeAnalysisVC else { return }
-                    navigationController?.pushViewController(secondVC, animated: true)
-                    
-                    // Modal로 띄우고 싶으면 NavigationController를 연결하는 선행 과정 없이 present 메서드를 사용하면 됨.
-                }
+                // 네번째 탭의 VC는 NavigationBar를 가지고 있어서 UINavigationController로 다운 캐스팅을 해주기
+                let navigationController = self.children[index] as? UINavigationController
                 
+                
+                // navigationController에 연결되어 있는 secondVC를 push 형식으로 전환
+                guard let secondVC = storyboard?.instantiateViewController(withIdentifier: "secondVC") as? BeforeAnalysisVC else { return }
+                navigationController?.pushViewController(secondVC, animated: true)
+                
+                // Modal로 띄우고 싶으면 NavigationController를 연결하는 선행 과정 없이 present 메서드를 사용하면 됨.
             }
+            
         }
+    }
     
     private func configureTableView(_ tableView: UITableView, cellClass: UITableViewCell.Type, identifier: String) {
         view.addSubview(tableView)
@@ -231,7 +230,7 @@ class MainVC: UIViewController{
             // Handle the navigation or any other action if the limit is not reached
             let actionItemController =  ActionItemController() // Replace this line with your desired action
             navigationController?.pushViewController(actionItemController, animated: true)
-       
+            
             
         }
     }
